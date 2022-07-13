ddb81dcf5e436979f2b1369f4ddb0e2565cdb42d
Jafar Asad <rafajdasa@gmail.com>
2022-07-13 18:46:30 -0400

    made changes to turtle_dodger and added sound

--- Turtle Dodger/turtle_dodger.py
+++ Turtle Dodger/turtle_dodger.py
@@ -1,10 +1,11 @@
 import turtle
+import winsound
 
 wn = turtle.Screen() 
     #wn is window
 wn.title("Test Game by TheTraveler")
 wn.bgcolor("black")
-wn.setup(width=800, height=600)
+wn.setup(width=400, height=600)
 wn.tracer(0)
 
 #Character "A" Apperance
@@ -79,7 +80,7 @@ arrow_7.color("red")
 arrow_7.penup()
 arrow_7.goto(-150,240)
 arrow_7.right(90)
-arrow_7.dy = -0.30
+arrow_7.dy = -0.32
 
 arrow_8 = turtle.Turtle()
 arrow_8.speed(0)
@@ -88,7 +89,7 @@ arrow_8.color("red")
 arrow_8.penup()
 arrow_8.goto(200,240)
 arrow_8.right(90)
-arrow_8.dy = -0.30
+arrow_8.dy = -0.40
 
 arrow_9 = turtle.Turtle()
 arrow_9.speed(0)
@@ -97,79 +98,7 @@ arrow_9.color("red")
 arrow_9.penup()
 arrow_9.goto(-200,240)
 arrow_9.right(90)
-arrow_9.dy = -0.30
-
-arrow_10 = turtle.Turtle()
-arrow_10.speed(0)
-arrow_10.shape("triangle")
-arrow_10.color("red")
-arrow_10.penup()
-arrow_10.goto(250,240)
-arrow_10.right(90)
-arrow_10.dy = -0.30
-
-arrow_11 = turtle.Turtle()
-arrow_11.speed(0)
-arrow_11.shape("triangle")
-arrow_11.color("red")
-arrow_11.penup()
-arrow_11.goto(-250,240)
-arrow_11.right(90)
-arrow_11.dy = -0.30
-
-arrow_12 = turtle.Turtle()
-arrow_12.speed(0)
-arrow_12.shape("triangle")
-arrow_12.color("red")
-arrow_12.penup()
-arrow_12.goto(300,240)
-arrow_12.right(90)
-arrow_12.dy = -0.30
-
-arrow_13 = turtle.Turtle()
-arrow_13.speed(0)
-arrow_13.shape("triangle")
-arrow_13.color("red")
-arrow_13.penup()
-arrow_13.goto(-300,240)
-arrow_13.right(90)
-arrow_13.dy = -0.30
-
-arrow_14 = turtle.Turtle()
-arrow_14.speed(0)
-arrow_14.shape("triangle")
-arrow_14.color("red")
-arrow_14.penup()
-arrow_14.goto(350,240)
-arrow_14.right(90)
-arrow_14.dy = -0.30
-
-arrow_15 = turtle.Turtle()
-arrow_15.speed(0)
-arrow_15.shape("triangle")
-arrow_15.color("red")
-arrow_15.penup()
-arrow_15.goto(-350,240)
-arrow_15.right(90)
-arrow_15.dy = -0.30
-
-arrow_16 = turtle.Turtle()
-arrow_16.speed(0)
-arrow_16.shape("triangle")
-arrow_16.color("red")
-arrow_16.penup()
-arrow_16.goto(400,240)
-arrow_16.right(90)
-arrow_16.dy = -0.30
-
-arrow_17 = turtle.Turtle()
-arrow_17.speed(0)
-arrow_17.shape("triangle")
-arrow_17.color("red")
-arrow_17.penup()
-arrow_17.goto(-400,240)
-arrow_17.right(90)
-arrow_17.dy = -0.30
+arrow_9.dy = -0.40
 
 #Pen
 pen = turtle.Turtle()
@@ -216,7 +145,6 @@ wn.onkeypress(char_a_right, "Right")
 def dead_turtle():
     for i in range(4):
         char_a.right(i)
-        
 
 
 
@@ -225,11 +153,11 @@ while True:
     wn.update()
 
     #Border Check
-    if char_a.xcor() > 380:
-        char_a.setx(380)
+    if char_a.xcor() > 180:
+        char_a.setx(180)
     
-    if char_a.xcor() < -380:
-        char_a.setx(-380)
+    if char_a.xcor() < -180:
+        char_a.setx(-180)
     
     #Move Arrow_1
     arrow_1.sety(arrow_1.ycor() + arrow_1.dy)
@@ -329,95 +257,8 @@ while True:
             arrow_9.goto(-200,240)
             score += 1
             pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))     
-
-    #Arrow_10 Activate and Reset
-    arrow_10.hideturtle()
-    if score >= 9:
-        arrow_10.showturtle()
-        arrow_10.sety(arrow_10.ycor() + arrow_10.dy)
-        if arrow_10.ycor() < -300:
-            arrow_10.goto(250,240)
-            score += 1
-            pen.clear()
             pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
 
-    #Arrow_11 Activate and Reset
-    arrow_11.hideturtle()
-    if score >= 10:
-        arrow_11.showturtle()
-        arrow_11.sety(arrow_11.ycor() + arrow_11.dy)
-        if arrow_11.ycor() < -300:
-            arrow_11.goto(-250,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))   
-
-    #Arrow_12 Activate and Reset
-    arrow_12.hideturtle()
-    if score >= 11:
-        arrow_12.showturtle()
-        arrow_12.sety(arrow_12.ycor() + arrow_12.dy)
-        if arrow_12.ycor() < -300:
-            arrow_12.goto(300,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_13 Activate and Reset
-    arrow_13.hideturtle()
-    if score >= 12:
-        arrow_13.showturtle()
-        arrow_13.sety(arrow_13.ycor() + arrow_13.dy)
-        if arrow_13.ycor() < -300:
-            arrow_12.goto(-300,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_14 Activate and Reset
-    arrow_14.hideturtle()
-    if score >= 13:
-        arrow_14.showturtle()
-        arrow_14.sety(arrow_14.ycor() + arrow_14.dy)
-        if arrow_14.ycor() < -300:
-            arrow_14.goto(350,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_15 Activate and Reset
-    arrow_15.hideturtle()
-    if score >= 14:
-        arrow_15.showturtle()
-        arrow_15.sety(arrow_15.ycor() + arrow_15.dy)
-        if arrow_15.ycor() < -300:
-            arrow_15.goto(-350,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_16 Activate and Reset
-    arrow_16.hideturtle()
-    if score >= 15:
-        arrow_16.showturtle()
-        arrow_16.sety(arrow_16.ycor() + arrow_16.dy)
-        if arrow_16.ycor() < -300:
-            arrow_16.goto(400,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_17 Activate and Reset
-    arrow_17.hideturtle()
-    if score >= 16:
-        arrow_17.showturtle()
-        arrow_17.sety(arrow_17.ycor() + arrow_17.dy)
-        if arrow_17.ycor() < -300:
-            arrow_17.goto(-400,240)
-            score += 1
-            pen.clear()
-            pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
 
     #Arrow_1 Collision
 
@@ -426,6 +267,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
     
     #Arrow_2 Collision
 
@@ -434,6 +276,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_3 Collision
 
@@ -442,6 +285,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_4 Collision
 
@@ -450,6 +294,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_5 Collision
 
@@ -458,6 +303,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_6 Collision
 
@@ -466,7 +312,8 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-    
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
+
     #Arrow_7 Collision
 
     if (arrow_7.ycor() < char_a.ycor() + 40) and (arrow_7.xcor() > char_a.xcor() - 30) and (arrow_7.xcor() < char_a.xcor() + 30):
@@ -474,6 +321,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_8 Collision
 
@@ -482,6 +330,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Arrow_9 Collision
 
@@ -490,70 +339,7 @@ while True:
         lives -= 1
         pen.clear()
         pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_10 Collision
-
-    if (arrow_10.ycor() < char_a.ycor() + 40) and (arrow_10.xcor() > char_a.xcor() - 30) and (arrow_10.xcor() < char_a.xcor() + 30):
-        arrow_10.goto(250, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_11 Collision
-
-    if (arrow_11.ycor() < char_a.ycor() + 40) and (arrow_11.xcor() > char_a.xcor() - 30) and (arrow_11.xcor() < char_a.xcor() + 30):
-        arrow_11.goto(-250, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_12 Collision
-
-    if (arrow_12.ycor() < char_a.ycor() + 40) and (arrow_12.xcor() > char_a.xcor() - 30) and (arrow_12.xcor() < char_a.xcor() + 30):
-        arrow_12.goto(300, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_13 Collision
-
-    if (arrow_13.ycor() < char_a.ycor() + 40) and (arrow_13.xcor() > char_a.xcor() - 30) and (arrow_13.xcor() < char_a.xcor() + 30):
-        arrow_13.goto(-300, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_14 Collision
-
-    if (arrow_14.ycor() < char_a.ycor() + 40) and (arrow_14.xcor() > char_a.xcor() - 30) and (arrow_14.xcor() < char_a.xcor() + 30):
-        arrow_14.goto(350, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_15 Collision
-
-    if (arrow_15.ycor() < char_a.ycor() + 40) and (arrow_15.xcor() > char_a.xcor() - 30) and (arrow_15.xcor() < char_a.xcor() + 30):
-        arrow_15.goto(-350, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_16 Collision
-
-    if (arrow_16.ycor() < char_a.ycor() + 40) and (arrow_16.xcor() > char_a.xcor() - 30) and (arrow_16.xcor() < char_a.xcor() + 30):
-        arrow_16.goto(400, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
-
-    #Arrow_17 Collision
-
-    if (arrow_17.ycor() < char_a.ycor() + 40) and (arrow_17.xcor() > char_a.xcor() - 30) and (arrow_17.xcor() < char_a.xcor() + 30):
-        arrow_17.goto(-400, 240)
-        lives -= 1
-        pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+        winsound.PlaySound("hit.wav", winsound.SND_ASYNC)
 
     #Game Over
 
@@ -564,8 +350,8 @@ while True:
         exit.write("GAME OVER", align="center", font=("Courier", 80, "bold"))        
         
         pen.clear()
-        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))        
-        
+        pen.write("Score: {}  Lives: {}".format(score, lives), align="center", font=("Courier", 24, "normal"))
+
         arrow_1.dy = 0
         arrow_2.dy = 0
         arrow_3.dy = 0
@@ -575,35 +361,16 @@ while True:
         arrow_7.dy = 0
         arrow_8.dy = 0
         arrow_9.dy = 0
-        arrow_10.dy = 0
-        arrow_11.dy = 0
-        arrow_12.dy = 0
-        arrow_13.dy = 0
-        arrow_14.dy = 0
-        arrow_15.dy = 0
-        arrow_16.dy = 0
-        arrow_17.dy = 0
+
+        arrow_1.goto(0, 240)
+        arrow_2.goto(50, 240)
+        arrow_3.goto(-50, 240)
+        arrow_4.goto(100, 240)
+        arrow_5.goto(-100, 240)
+        arrow_6.goto(150, 240)
+        arrow_7.goto(-150, 240)
+        arrow_8.goto(200, 240)
+        arrow_9.goto(-200, 240)
 
         dead_turtle()
-        # arrow_1.hideturtle()
-        # arrow_2.hideturtle()
-        # arrow_3.hideturtle()
-        # arrow_4.hideturtle()
-        # arrow_5.hideturtle()
-        # arrow_6.hideturtle()
-        # arrow_7.hideturtle()
-        # arrow_8.hideturtle()
-        # arrow_9.hideturtle()
-        # arrow_10.hideturtle()
-        # arrow_11.hideturtle()
-        # arrow_12.hideturtle()
-        # arrow_13.hideturtle()
-        # arrow_14.hideturtle()
-        # arrow_15.hideturtle()
-        # arrow_16.hideturtle()
-        # arrow_17.hideturtle()              
-
-
-        
- 
 
